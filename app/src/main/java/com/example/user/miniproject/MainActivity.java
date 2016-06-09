d3b2be4c7f821991e046321697310eb0c85f7796
lihiv <lihiv92@gmail.com>
2016-06-09 17:50:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/miniproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.miniproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected int testUpload(int i){
+        return i;
+    }
+}
